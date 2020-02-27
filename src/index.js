3644f03e12b2b0bf4d9495f57129c735b19873e4
cagpie <cagpie@gmail.com>
2020-02-27 20:36:53 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import drawTextWithTwemoji from './drawTextWithTwemoji';
 
-export async function fillTextWithTwemoji (context, text, x, y, options?) {
+export async function fillTextWithTwemoji (context, text, x, y, options = {}) {
   return await drawTextWithTwemoji(context, 'fill', text, x, y, options);
 }
 
-export async function strokeTextWithTwemoji (context, text, x, y, options) {
+export async function strokeTextWithTwemoji (context, text, x, y, options = {}) {
   return await drawTextWithTwemoji(context, 'stroke', text, x, y, options);
 }
 
-export function measureText (context, text, options) {
+export function measureText (context, text, options = {}) {
   
 }
